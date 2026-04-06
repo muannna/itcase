0be90d4191fa4b865a0e8cf5c87f117bbddd050d
muannna <evagoeshard@gmail.com>
2026-04-06 15:55:08 +0500

    feat: add in-stock filter with URL state

--- src/pages/products/ProductsPage.jsx
+++ src/pages/products/ProductsPage.jsx
@@ -1,8 +1,13 @@
 import { useProducts } from '../../queries/products/useProducts'
+import { isProductInStock } from '../../utils/isProductInStock'
 import { ProductCard } from './components/ProductCard'
+import { useSearchParams } from 'react-router-dom'
 
 export function ProductsPage() {
   const { data: products, isLoading, error } = useProducts()
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const inStock = searchParams.get('inStock')
 
   if (isLoading) {
     return <p>Loading products...</p>
@@ -21,14 +26,38 @@ export function ProductsPage() {
     )
   }
 
+  const filteredProductes = products.filter((product) => {
+    if (!inStock) return true
+
+    return isProductInStock(product)
+  })
+
   return (
-    <div>
-      <h1>Products</h1>
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={inStock === 'true'}
+          onChange={(e) => {
+            const params = new URLSearchParams(searchParams)
+            if (e.target.checked) {
+              params.set('inStock', 'true')
+            } else {
+              params.delete('inStock')
+            }
+            setSearchParams(params)
+          }}
+        />
+        In Stock Only
+      </label>
       <div>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+        <h1>Products</h1>
+        <div>
+          {filteredProductes.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
