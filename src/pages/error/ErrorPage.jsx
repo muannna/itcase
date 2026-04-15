680a35cc794c4b89e83a0f0c9d63e25ff93ff924
muannna <evagoeshard@gmail.com>
2026-04-16 02:56:40 +0500

    fix: add central positioning to empty state and error component

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,20 +1,16 @@
 import { useRouteError } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
 import { Error } from '../../shared/ui/error/Error'
-import { Button } from '../../shared/ui/button/Button'
 
 import styles from './ErrorPage.module.css'
 
 export function ErrorPage() {
   const error = useRouteError()
-  const navigate = useNavigate()
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Oops!</h1>
       <p className={styles.subtitle}>Sorry, an unexpected error has occurred.</p>
       <Error error={error} />
-      <Button onClick={() => navigate('/')}>Go home</Button>
     </div>
   )
 }
--- src/shared/ui/error/Error.jsx
+++ src/shared/ui/error/Error.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+import { Button } from '../button/Button'
+
 import styles from './Error.module.css'
 
 export function Error({ error, message }) {
+  const navigate = useNavigate()
+
   return (
-    <p className={styles.error}>{`${error.statusText || message || 'Something went wrong'}`}</p>
+    <div className={styles.errorContainer}>
+      <p className={styles.error}>{`${error.statusText || message || 'Something went wrong'}`}</p>
+      <Button onClick={() => navigate('/')}>Go home</Button>
+    </div>
   )
 }
