819ed4ef72cbffe6a8cbf5decf717843f5117789
muannna <evagoeshard@gmail.com>
2026-04-17 12:09:14 +0500

    refactor: simplify cart helpers

--- src/app/store/cart/cartHelpers.js
+++ src/app/store/cart/cartHelpers.js
@@ -7,15 +7,3 @@ export const removeItemHelper = (state, id) => {
   const index = state.items.findIndex((el) => el.id === id)
   if (index !== -1) state.items.splice(index, 1)
 }
-
-export const increaseQuantityHelper = (item) => {
-  item.quantity += 1
-}
-
-export const decreaseQuantityHelper = (item) => {
-  if (item.quantity <= 1) return
-  item.quantity -= 1
-}
-
-export const findItemInCartHelper = (cart, { productId, colorId, sizeId }) =>
-  cart.find((i) => i.productId === productId && i.colorId === colorId && i.sizeId === sizeId)
--- src/hooks/cart/useCartItem.js
+++ src/hooks/cart/useCartItem.js
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux'
 import { selectCartItems } from '../../app/store/cart/selectors'
-import { findItemInCartHelper } from '../../app/store/cart/cartHelpers'
+import { createCartItemIdHelper } from '../../app/store/cart/cartHelpers'
 
 export function useCartItem({ productId, colorId, sizeId }) {
   const cartItems = useSelector(selectCartItems)
@@ -11,11 +11,9 @@ export function useCartItem({ productId, colorId, sizeId }) {
       quantity: 0,
     }
   }
-  const item = findItemInCartHelper(cartItems, {
-    productId,
-    colorId,
-    sizeId,
-  })
+
+  const id = createCartItemIdHelper({ productId, colorId, sizeId })
+  const item = cartItems.find((item) => item.id === id)
 
   return {
     isInCart: Boolean(item),
